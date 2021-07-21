49276d8c5200e3fe57dbca0d32b8a57bfa49dbf9
mihalyilevente <87650420+mihalyilevente@users.noreply.github.com>
2021-07-21 14:20:31 +0200

    Update Program.cs

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -7,6 +7,7 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string userName = Console.ReadLine();
         }
     }
 }
