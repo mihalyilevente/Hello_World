439d0897c375d5ada3e193c1087736bf37bcfec5
mihalyilevente <87650420+mihalyilevente@users.noreply.github.com>
2021-07-22 13:57:09 +0200

    Update Program.cs

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -8,6 +8,13 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Sleep for 2 seconds.");
+            Thread.Sleep(20000);
+        }
+
+        Console.WriteLine("Main thread exits.");
         }
     }
 }
